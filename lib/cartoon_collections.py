03c1c27a240041e8938fa949d820b39d4e4c8835
Joe Milius <joemilius@gmail.com>
2022-10-24 22:56:48 -0600

    completed

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,30 @@
-def roll_call_dwarves():
+def roll_call_dwarves(dwarves):
+    order_number = 1
+    for dwarf in dwarves:
+        print(f'{order_number}. {dwarf}')
+        order_number += 1
     pass
 
-def summon_captain_planet():
+def summon_captain_planet(calls):
+    i = 0
+    while i < (len(calls)):
+        calls[i] = calls[i][0].upper() + f'{calls[i][1:]}!'
+        i += 1
+    
+    return calls
     pass
 
-def long_planeteer_calls():
+def long_planeteer_calls(words):
+    for word in words:
+        if(len(word) > 4):
+            return True
+    
+    return False
     pass
 
-def find_the_cheese():
+def find_the_cheese(foods):
+    cheeses = ["cheddar", 'gouda', 'camembert']
+    for cheese in cheeses:
+        if cheese in foods:
+            return cheese
     pass
